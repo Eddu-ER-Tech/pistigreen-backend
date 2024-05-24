7e4cade33b0b68fe9de5b3c0c7d8f4dc7d035562
Edwin Rojas <168507810+Eddu-ER-Tech@users.noreply.github.com>
2024-05-24 16:19:30 -0500

    Crreacion de app search

--- post/api.py
+++ post/api.py
@@ -3,13 +3,13 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from account.models import User, FriendshipRequest
+from account.models import User #FriendshipRequest
 from account.serializers import UserSerializer
-from notification.utils import create_notification
+# from notification.utils import create_notification
 
-from .forms import PostForm, AttachmentForm
-from .models import Post, Like, Comment, Trend
-from .serializers import PostSerializer, PostDetailSerializer, CommentSerializer, TrendSerializer
+from .forms import PostForm #AttachmentForm
+from .models import Post #Like, Comment, Trend
+from .serializers import PostSerializer #PostDetailSerializer, CommentSerializer, TrendSerializer
 
 
 @api_view(['GET'])
@@ -61,11 +61,11 @@ def post_list_profile(request, id):
     if request.user in user.friends.all():
         can_send_friendship_request = False
     
-    check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(created_by=user)
-    check2 = FriendshipRequest.objects.filter(created_for=user).filter(created_by=request.user)
+    # check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(created_by=user)
+    # check2 = FriendshipRequest.objects.filter(created_for=user).filter(created_by=request.user)
 
-    if check1 or check2:
-        can_send_friendship_request = False
+    # if check1 or check2:
+    #     can_send_friendship_request = False
 
     return JsonResponse({
         'posts': posts_serializer.data,
@@ -123,41 +123,41 @@ def post_like(request, pk):
         return JsonResponse({'message': 'post already liked'})
 
 
-@api_view(['POST'])
-def post_create_comment(request, pk):
-    comment = Comment.objects.create(body=request.data.get('body'), created_by=request.user)
+# @api_view(['POST'])
+# def post_create_comment(request, pk):
+#     comment = Comment.objects.create(body=request.data.get('body'), created_by=request.user)
 
-    post = Post.objects.get(pk=pk)
-    post.comments.add(comment)
-    post.comments_count = post.comments_count + 1
-    post.save()
+#     post = Post.objects.get(pk=pk)
+#     post.comments.add(comment)
+#     post.comments_count = post.comments_count + 1
+#     post.save()
 
-    notification = create_notification(request, 'post_comment', post_id=post.id)
+#     notification = create_notification(request, 'post_comment', post_id=post.id)
 
-    serializer = CommentSerializer(comment)
+#     serializer = CommentSerializer(comment)
 
-    return JsonResponse(serializer.data, safe=False)
-
-
-@api_view(['DELETE'])
-def post_delete(request, pk):
-    post = Post.objects.filter(created_by=request.user).get(pk=pk)
-    post.delete()
-
-    return JsonResponse({'message': 'post deleted'})
+#     return JsonResponse(serializer.data, safe=False)
 
 
-@api_view(['POST'])
-def post_report(request, pk):
-    post = Post.objects.get(pk=pk)
-    post.reported_by_users.add(request.user)
-    post.save()
+# @api_view(['DELETE'])
+# def post_delete(request, pk):
+#     post = Post.objects.filter(created_by=request.user).get(pk=pk)
+#     post.delete()
 
-    return JsonResponse({'message': 'post reported'})
+#     return JsonResponse({'message': 'post deleted'})
 
 
-@api_view(['GET'])
-def get_trends(request):
-    serializer = TrendSerializer(Trend.objects.all(), many=True)
+# @api_view(['POST'])
+# def post_report(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     post.reported_by_users.add(request.user)
+#     post.save()
 
-    return JsonResponse(serializer.data, safe=False)
+#     return JsonResponse({'message': 'post reported'})
+
+
+# @api_view(['GET'])
+# def get_trends(request):
+#     serializer = TrendSerializer(Trend.objects.all(), many=True)
+
+#     return JsonResponse(serializer.data, safe=False)
